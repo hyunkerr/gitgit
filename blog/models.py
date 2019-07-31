ddfd01173414a39194dbae2416cd9ed51e5df4a3
hyunkerr <shg7788@likelion.org>
2019-07-31 19:33:45 +0900

    first

---
+++ blog/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=200)
+    pub_date=models.DateTimeField(default=timezone.now())
+    body=models.TextField()
